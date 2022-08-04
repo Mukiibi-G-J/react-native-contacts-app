268ddc96bd9d69b5e2b8b656677e2ca7436a2923
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-04 13:36:43 +0300

    setting up button component

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,6 +2,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import Container from '../../components/common/Container';
 import Input from '../../components/common/input';
+import CustomButton from '../../components/common/CustomButton';
 
 export const Login = () => {
   const [value, onChangeText] = React.useState('');
@@ -21,8 +22,11 @@ export const Login = () => {
         icon={<Text>@</Text>}
         value={value}
         iconPosition="right"
-        // error={'This field is required'}
+        // error={'This field is requi red'}
       />
+      <CustomButton title="submit" primary loading={true} />
+      <CustomButton title="submit" primary loading={true} />
+      <CustomButton title="submit" danger loading={false} />
     </Container>
   );
 };
